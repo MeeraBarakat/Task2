e0da9e5c0835242fe847c4648b09abaa977a5949
MeeraBarakat <meera.gh.barakat@gmail.com>
2022-11-01 16:29:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useEffect, useState,useRef} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const ProgressBar = (props) => {
+  const [progress,setProgress] = useState(0);
+
+   
+    useEffect(()=>{
+      
+setTimeout(function(props) {
+  if(progress<100)
+  {
+    setProgress(progress+10)
+  }
+}, 1000);
+    },[progress]);
+ 
+  return (
+    <div className='progreddContainer'>   
+  <div className='innerContainer' style={{width:`${progress}%`,  backgroundColor: `${props.item.barColor}`}} >
+  <span className='percentage'>{`${progress}%`}</span>
+  </div>
+</div>
+  );
+};
 
 function App() {
+  const [bars,setBars] = useState([{barColor:"red"}]);
+  const inColor=useRef('');
+
+  function handleClick(){
+    console.log();
+    setBars(bars =>[...bars,{barColor:inColor.current.value}])
+    console.log(bars.length)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className='myRoot'>
+    <label>Color:</label>
+    <input ref={inColor}></input>
+    <label>Title:</label>
+    <input ref={inColor}></input>
+    <button className="btn btn-outline-primary btn-sm addP" onClick={handleClick}>Add progress</button>
+    </div>
+     <div>
+    {bars.map(item=>(
+    <ProgressBar item={item} />
+    ))}
+    </div>
     </div>
   );
 }
